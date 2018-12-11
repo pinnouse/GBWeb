6ea5c89c287f2c212595ea86964bc90b2c2fbbcb
pinnouse <cupofpho@gmail.com>
2018-12-11 17:43:08 -0500

    External sources first

--- zerotwo/index.php
+++ zerotwo/index.php
@@ -28,13 +28,13 @@ curl_close($ch);
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 
-  <link rel="stylesheet" type="text/css" href="zerotwo/style.css">
-  <script src="zerotwo/index.js"></script>
-  
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
   <link href="https://fonts.googleapis.com/css?family=Noto+Sans|Oxygen+Mono" rel="stylesheet">
+
+  <link rel="stylesheet" type="text/css" href="zerotwo/style.css">
+  <script src="zerotwo/index.js"></script>
 </head>
 <body>
   <?php
